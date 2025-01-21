42fb4f22a9750cbb53a0dc776c62aeee9009466d
Dylan Collins <pyxyllv@gmail.com>
2025-01-21 18:56:24 +0000

    Build issues

--- app/page.js
+++ app/page.js
@@ -1,14 +1,12 @@
+import { Suspense } from 'react'
 import TimetableApp from '@/components/TimetableApp'
-
-export const metadata = {
-  title: 'College Timetable',
-  description: 'A simple timetable application for college students',
-}
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-gray-50">
-      <TimetableApp />
+    <main>
+      <Suspense fallback={<div>Loading...</div>}>
+        <TimetableApp />
+      </Suspense>
     </main>
   )
 }
