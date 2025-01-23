f56dc45f5d1ff5918a2e250f7256016a9af1f8f2
Dylan Collins <pyxyllv@gmail.com>
2025-01-23 19:30:54 +0000

    Formatting fix

--- app/icon.jsx
+++ app/icon.jsx
@@ -1,13 +1,13 @@
 import { ImageResponse } from 'next/og'
- 
+
 export const runtime = 'nodejs'
- 
+
 export const size = {
   width: 32,
   height: 32,
 }
 export const contentType = 'image/png'
- 
+
 export default function Icon() {
   return new ImageResponse(
     (
